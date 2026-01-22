25e4420c4882ce1a411f5895b1bd2a190b131e6c
karencastrejon10 <karencastrejon15@gmail.com>
2026-01-22 12:47:23 -0600

    Agregue id para que detectara cada producto y poder hacelo dinamico, que al darle clic a un producto te muestre diferentes datos

--- section_perros_adulto.js
+++ section_perros_adulto.js
@@ -1,17 +1,17 @@
 // Array de productos
 const productos = [
-  { nombre: "Beneful", precio: 503, img:  "IMAGENES/4.PERROS/Beneful.jpg" },
-  { nombre: "Dog Chow", precio: 595, img: "IMAGENES/4.PERROS/DogChow.jpg" },
-  { nombre: "Nupec Perro Senior", precio: 500, img: "IMAGENES/4.PERROS/Nupec Perro.jpg" },
-  { nombre: "Nupec razas pequeñas", precio: 650, img: "IMAGENES/4.PERROS/nupec2.jpg" },
-  { nombre: "Orije", precio: 1775, img: "IMAGENES/4.PERROS/Orijen Perro2.jpg" },
-  { nombre: "Purina ONE razas pequeñas", precio: 700, img: "IMAGENES/4.PERROS/purina_one1.jpg" },
-  {nombre:"Purina ONE Adultos",precio: 300,img:"IMAGENES/4.PERROS/purina_one2.jpg"},
-  {nombre:"Purina ONE Senior",precio: 450,img:"IMAGENES/4.PERROS/purina_one3.jpg"},
-  {nombre:"Royal Canin Perro Mediano",precio: 750,img:"IMAGENES/4.PERROS/Royal Canin Perro.jpg"},
-  {nombre:"Royal Canin Adulto",precio: 750,img:"IMAGENES/4.PERROS/Royal Canin Perro2.jpg"},
-  {nombre:"Taste Food Perro Carne",precio: 1750,img:"IMAGENES/4.PERROS/Taste Food Perro1.webp"},
-  {nombre:"Taste Food Perro Pescado",precio:1200,img:"IMAGENES/4.PERROS/Taste Food Perro3.jpg"}
+  { id: 0, nombre: "Beneful", precio: 503, img:  "IMAGENES/4.PERROS/Beneful.jpg" },
+  { id: 1, nombre: "Dog Chow", precio: 595, img: "IMAGENES/4.PERROS/DogChow.jpg" },
+  { id: 2, nombre: "Nupec Perro Senior", precio: 500, img: "IMAGENES/4.PERROS/Nupec Perro.jpg" },
+  { id: 3, nombre: "Nupec razas pequeñas", precio: 650, img: "IMAGENES/4.PERROS/nupec2.jpg" },
+  { id: 4, nombre: "Orije", precio: 1775, img: "IMAGENES/4.PERROS/Orijen Perro2.jpg" },
+  { id: 5, nombre: "Purina ONE razas pequeñas", precio: 700, img: "IMAGENES/4.PERROS/purina_one1.jpg" },
+  { id: 6, nombre:"Purina ONE Adultos",precio: 300,img:"IMAGENES/4.PERROS/purina_one2.jpg"},
+  { id: 7, nombre:"Purina ONE Senior",precio: 450,img:"IMAGENES/4.PERROS/purina_one3.jpg"},
+  { id: 8, nombre:"Royal Canin Perro Mediano",precio: 750,img:"IMAGENES/4.PERROS/Royal Canin Perro.jpg"},
+  { id: 9, nombre:"Royal Canin Adulto",precio: 750,img:"IMAGENES/4.PERROS/Royal Canin Perro2.jpg"},
+  { id: 10, nombre:"Taste Food Perro Carne",precio: 1750,img:"IMAGENES/4.PERROS/Taste Food Perro1.webp"},
+  { id: 11, nombre:"Taste Food Perro Pescado",precio:1200,img:"IMAGENES/4.PERROS/Taste Food Perro3.jpg"}
 ];
 
 // Seleccionamos el contenedor
@@ -23,10 +23,10 @@ productos.forEach(producto => {
   div.classList.add("producto");
 
   div.innerHTML = `
-   <a href="p_adulto1.html"><img src="${producto.img}" alt="${producto.nombre}"></a>
-    <a href="p_adulto1.html"><h3>${producto.nombre}</h3></a>
+   <a href="p_adulto1.html?id=${producto.id}"><img src="${producto.img}" alt="${producto.nombre}"></a>
+    <a href="p_adulto1.html?id=${producto.id}"><h3>${producto.nombre}</h3></a>
     <p>Desde $${producto.precio} MXN</p>
-    <button><a href="p_adulto1.html">Comprar</a></button>
+    <button><a href="p_adulto1.html?id=${producto.id}">Comprar</a></button>
   `;
 
   grid.appendChild(div);
